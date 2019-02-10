f30db296ce54d2e43f128c472237c66126e29e18
ItayTur <unlessmusic1@gmail.com>
2019-02-10 11:13:17 +0200

    Create BurgerBuilder.js

---
+++ vegan-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+
+
+    render() {
+        return (
+            <>
+                <div>Burger</div>
+                <div>Burger Control</div>
+            </>
+        );
+    }
+}
+
+export default BurgerBuilder;
