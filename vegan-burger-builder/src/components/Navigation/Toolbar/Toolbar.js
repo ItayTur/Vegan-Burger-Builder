8e5866f50b98c1a4d701b58cdfea98bfa84e4643
ItayTur <unlessmusic1@gmail.com>
2019-02-11 17:34:26 +0200

    Create logo

--- vegan-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ vegan-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
 
 const toolbar = (props) => {
-    return (<header className={classes.Toolbar}>
+    return (
+    <header className={classes.Toolbar}>
         <div>MANU</div>
-        <div>LOGO</div>
+        <Logo/>
         <nav>
             ...
         </nav>
-    </header>)
+    </header>);
 }
 
 export default toolbar;
