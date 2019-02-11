a64c6c82675436efa63b1585ab2a96c905118d2a
ItayTur <unlessmusic1@gmail.com>
2019-02-11 11:33:35 +0200

    Order summery

---
+++ vegan-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const orderSummary = (props) => {
+    const orderDetails = Object.keys(props.ingredients)
+    .map(ingKey => <li><span style={{textTransform: 'capitalize'}}>{ingKey}:</span> 
+    {props.ingredients[ingKey]}</li>) 
+    return (
+        <>
+            <h3>Your Order</h3>
+            <span >A delicious burger with the following ingredients:</span>
+            <ul>
+                {orderDetails}
+            </ul>
+            Would you like to check out?
+        </>
+    );
+}
+
+export default orderSummary;
