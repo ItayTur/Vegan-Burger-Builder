f40665ce8345de93afa3e409ccb7cdec239d88a9
ItayTur <unlessmusic1@gmail.com>
2019-10-11 15:47:30 +0300

    add redux dev tools and redux-thunk middleware
    in order to work with async code in redux i install the redux-thunk npm lib for the thunk middleware that handles async code in redux.

--- vegan-burger-builder/src/index.js
+++ vegan-burger-builder/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(burgerBuilderReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={store}>
