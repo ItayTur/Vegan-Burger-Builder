f280dbec569da303247be9d4252dc0589c283cac
ItayTur <unlessmusic1@gmail.com>
2019-10-26 20:41:42 +0300

    Update orders.js

--- vegan-burger-builder/src/store/reducers/orders.js
+++ vegan-burger-builder/src/store/reducers/orders.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     orders: [],
@@ -6,49 +7,56 @@ const initialState = {
     isOrderSuccess: false
 }
 
+const orderInit = (state, action) => {
+    return updateObject(state, { isOrderSuccess: false })
+}
+
+const startOrder = (state, action) => {
+    return updateObject(state, { loading: true })
+}
+
+const onOrderSuccess = (state, action) => {
+    const newOrder = {
+        id: action.id,
+        data: action.orderData
+    }
+
+    const newOrders = state.orders.concat(newOrder);
+    return updateObject(
+        state, {
+        loading: false,
+        orders: newOrders,
+        isOrderSuccess: true
+    })
+}
+
+const startFetchOrders = (state, action) => {
+    return updateObject(state, { loading: true });
+}
+
+const fetchOrdersSuccess = (state, action) => {
+    return updateObject(state, {
+        loading: false,
+        orders: action.orders
+    })
+}
+
+const fetchOrderdFails = (state, action) => {
+    return updateObject(state, {
+        loading: false,
+        isOrderSuccess: false
+    })
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ORDER_INIT:
-            return {
-                ...state,
-                isOrderSuccess: false
-            }
-        case actionTypes.START_ORDER:
-            return {
-                ...state,
-                loading: true
-            }
-        case actionTypes.ON_ORDER_SUCCESS:
-            const newOrder = {
-                id: action.id,
-                order: action.orderData
-            }
-            return {
-                ...state,
-                loading: false,
-                orders: state.orders.concat(newOrder),
-                isOrderSuccess: true
-            }
-        case actionTypes.START_FETCH_ORDERS:
-            return {
-                ...state,
-                loading: true
-            }
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                isOrderSuccess: true,
-                orders: action.orders
-            }
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return {
-                ...state,
-                loading: false,
-                isOrderSuccess: false
-            }
-        default:
-            return state;
+        case actionTypes.ORDER_INIT: return orderInit(state, action);
+        case actionTypes.START_ORDER: return startOrder(state, action);
+        case actionTypes.ON_ORDER_SUCCESS: return onOrderSuccess(state, action);
+        case actionTypes.START_FETCH_ORDERS: return startFetchOrders(state, action);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrderdFails(state, action);
+        default: return state;
     }
 }
 
